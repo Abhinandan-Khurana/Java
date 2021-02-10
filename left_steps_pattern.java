16df3e1f9fe3fe135178bf0a1c196b2c9a98c272
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2021-02-10 17:01:41 +0530

    Create left_steps_pattern.java

---
+++ left_steps_pattern.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class left_steps_pattern
+{
+    public static void main(System[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,n;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
